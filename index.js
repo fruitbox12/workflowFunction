0b38533529b8ed720f6d2f05018058d6ae54a08e
fruitbox12 <dylanwong007@gmail.com>
2024-02-09 19:15:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const product = require("./api/product");
 app.use(cors({
     origin: '*', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these methods
-    allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
+    allowedHeaders: ['X-Tenant-ID','Content-Type', 'Authorization'] // Allow these headers
   }));
 app.use(express.json({ extended: false }));
 

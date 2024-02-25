8ef5142565c944c7457f4921dc2af442351f64f6
fruitbox12 <dylanwong007@gmail.com>
2024-02-25 13:11:49 -0800

    Update workflow.js

--- api/workflow.js
+++ api/workflow.js
@@ -265,7 +265,7 @@ const stepEndValue = flowDataObj.nodes.length;
         
         // Prepare the body data for the webhook
         // This is just an example, adjust according to your actual data structure and needs
-        const bodyData =  JSON.stringify(workflow.flowData); // Assuming you want to send the entire flowData object
+        const bodyData =  workflow.flowData; // Assuming you want to send the entire flowData object
        
 
         // Execute the webhook using axios

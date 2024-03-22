4744da1fe47bfc8bd88180aaed9cdb43d715abe5
fruitbox12 <dylanwong007@gmail.com>
2024-03-21 23:55:25 -0700

    Update workflow.js

--- api/workflow.js
+++ api/workflow.js
@@ -270,7 +270,8 @@ const stepEndValue = flowDataObj.nodes.length;
        
 
         // Execute the webhook using axios
-    axios.post(webhookUrl, flowDataObj).then(webhookResponse => {
+    axios.post(webhookUrl, flowDataObj,         headers: { 'Content-Type': 'application/json' }
+).then(webhookResponse => {
     // Log the response data from the webhook
 
     // Respond with success and the data received from the webhook

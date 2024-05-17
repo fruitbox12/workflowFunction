2a45aaadd498ad070cdf19507723000b8e87d7f7
fruitbox12 <dylanwong007@gmail.com>
2024-05-16 20:22:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require("cors");
 
 const workflow = require("./api/workflow");
+const v2 = require("./api/workflow2");
+
 app.use(cors({
     origin: '*', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these methods
@@ -11,6 +13,7 @@ app.use(cors({
 app.use(express.json({ extended: false }));
 
 app.use("/api/v1", workflow);
+app.use("/api/v2", v2);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
